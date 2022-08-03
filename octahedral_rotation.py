1b01a8f699ddfae876a6815ad2a7aa146023255e
Sean <koysean@gmail.com>
2022-08-03 14:37:09 -0500

    Implemented find_MO_bonds - returns bond pairs and vectors.

--- octahedral_rotation.py
+++ octahedral_rotation.py
@@ -3,6 +3,7 @@ orthorhombic perovskite structure.
 
 Functions:
     standardize_atoms(xtl: ase.Atoms, to_primitive: bool = True)
+    find_MO_bonds(xtl: ase.Atoms)
 '''
 import ase
 import ase.geometry.geometry as geom
@@ -12,20 +13,24 @@ import ase.spacegroup as spacegroup
 import numpy as np
 import numpy.typing as npt
 import spglib
+from typing import Any
 
 anions = ["O", "S", "Se"] # anions to search for bonds
+cations = ["Ti", "Zr", "Hf"]
 
 def standardize_atoms(xtl: ase.Atoms, to_primitive: bool = True)  -> ase.Atoms:
-    ''' Converts ASE Atoms object to standard cell via spglib
+    ''' Converts ASE Atoms object to standard cell using spglib
 
     Parameters:
         xtl (ase.Atoms)
         to_primitive (bool)
 
     Returns:
-        cell, positions, chemical_numbers (tuple(ndarray[np.float64],
-            ndarray[np.float64], ndarray.int32)):
-        '''
+        (cell, positions, chemical_numbers)
+            cell (ndarray[np.float64])
+            positions (ndarray[np.float64])
+            chemical_numbers (ndarray[int32])
+    '''
     # Using spglib to convert xtl to standardized cell
     spglib_cell = (xtl.cell, xtl.get_scaled_positions(), xtl.numbers)
     standardized_cell = spglib.standardize_cell(spglib_cell, to_primitive=to_primitive)
@@ -55,8 +60,39 @@ def standardize_atoms(xtl: ase.Atoms, to_primitive: bool = True)  -> ase.Atoms:
 
     return xtl_std
 
-def find_MO_bonds(xtl: ase.Atoms):
+def find_MO_bonds(xtl: ase.Atoms) -> tuple[npt.NDArray[np.int32], npt.NDArray[Any]]:
+    ''' Finds B-site cation-anion pairs and the vectors connecting them.
+
+    Parameters:
+        xtl (ase.Atoms)
+
+    Returns:
+        bonds (Tuple[ndarray[int], ndarray[float]])
+    '''
     xtl_std = standardize_atoms(xtl)
+    # For each nearest neighbor ("bonded") pair:
+    #   i: ion 1 index
+    #   j: ion 2 index
+    #   D: distance vector
     cutoff = ase.neighborlist.natural_cutoffs(xtl_std)
     i, j, D = ase.neighborlist.neighbor_list('ijD', xtl_std, cutoff=cutoff)
-    return i, j, D
+
+    # find cation and anion indices
+    cation_indices = np.where(
+            [atom in cations for atom in xtl_std.get_chemical_symbols()]
+            )[0]
+    anion_indices = np.where(
+            [atom in anions for atom in xtl_std.get_chemical_symbols()]
+            )[0]
+
+    # enumerate valid B-site cation-anion bonds
+    bond_pairs = [] # populate with bond pairs [ion1, ion2]
+    bond_distances = [] # populate with distance vectors [x, y, z]
+    for idx, atom in enumerate(i):
+        # only count bond pairs where central atom is an anion and bonded
+        # atom is a B-site cation
+        if (atom in anion_indices) and (j[idx] in cation_indices):
+            bond_pairs.append([atom, j[idx]])
+            bond_distances.append(D[idx])
+
+    return (np.array(bond_pairs), np.array(bond_distances))
