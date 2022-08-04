abdebb2cb3ce8aca3516054ee3f48b384e5d0cd5
Sean <koysean@gmail.com>
2022-08-03 23:36:42 -0500

    Implemented bond_angle() calculations. Changed xtl->atoms.

--- octahedral_rotation.py
+++ octahedral_rotation.py
@@ -2,29 +2,33 @@
 orthorhombic perovskite structure.
 
 Functions:
-    standardize_atoms(xtl: ase.Atoms, to_primitive: bool = True)
-    find_MO_bonds(xtl: ase.Atoms)
-    pseudocubic_lattice_vectors(xtl: ase.Atoms)
+    standardize_atoms(atoms: ase.Atoms, to_primitive: bool = True)
+    find_MO_bonds(atoms: ase.Atoms)
+    pseudocubic_lattice_vectors(atoms: ase.Atoms)
     vector_projection(vector: npt.ArrayLike, normal: npt.ArrayLike)
+    bond_angle(bond1: npt.ArrayLike, bond2: npt.ArrayLike, proj_normal: npt.ArrayLike)
 '''
+
 import ase
 import ase.geometry.geometry as geom
 import ase.io
 import ase.neighborlist
-import ase.spacegroup as spacegroup
 import numpy as np
 import numpy.typing as npt
 import spglib
-from typing import Any
 
 anions = ["O", "S", "Se"]
 cations = ["Ti", "Zr", "Hf"] # B site cations
 
-def standardize_atoms(xtl: ase.Atoms, to_primitive: bool = True) -> ase.Atoms:
+class OctahedralRotations():
+    def __init__(self, atoms: ase.Atoms) -> None:
+        self.atoms = atoms
+
+def standardize_atoms(atoms: ase.Atoms, to_primitive: bool = True) -> ase.Atoms:
     ''' Converts ASE Atoms object to standard cell using spglib
 
     Parameters:
-        xtl (ase.Atoms)
+        atoms (ase.Atoms)
         to_primitive (bool)
 
     Returns:
@@ -33,15 +37,15 @@ def standardize_atoms(xtl: ase.Atoms, to_primitive: bool = True) -> ase.Atoms:
             positions (ndarray[np.float64])
             chemical_numbers (ndarray[int32])
     '''
-    # Using spglib to convert xtl to standardized cell
-    spglib_cell = (xtl.cell, xtl.get_scaled_positions(), xtl.numbers)
+    # Using spglib to convert atoms to standardized cell
+    spglib_cell = (atoms.cell, atoms.get_scaled_positions(), atoms.numbers)
     standardized_cell = spglib.standardize_cell(spglib_cell, to_primitive=to_primitive)
 
     # If spglib.standardize_cell() doesn't return a value, catch the error
     if not standardized_cell:
-        raise ValueError("spglib.standardize_cell() returned None; check that xtl is a valid ase.Atoms object.")
+        raise ValueError("spglib.standardize_cell() returned None; check that atoms is a valid ase.Atoms object.")
 
-    xtl_std = ase.Atoms(
+    atoms_std = ase.Atoms(
             cell=standardized_cell[0],
             scaled_positions=standardized_cell[1],
             symbols=standardized_cell[2],
@@ -49,42 +53,45 @@ def standardize_atoms(xtl: ase.Atoms, to_primitive: bool = True) -> ase.Atoms:
             )
 
     # permute axes (if necessary) so that c is the long axis.
-    lp = xtl_std.cell.lengths() # lattice parameters
+    lp = atoms_std.cell.lengths() # lattice parameters
 
     if np.argmax(lp) == 2: # no permutation needed
-        return xtl_std
+        return atoms_std
     elif np.argmax(lp) == 0:
         permute = [1, 2, 0]
     else:
         permute = [2, 0, 1]
 
-    xtl_std = geom.permute_axes(xtl_std, permute)
+    atoms_std = geom.permute_axes(atoms_std, permute)
 
-    return xtl_std
+    return atoms_std
 
-def find_MO_bonds(xtl: ase.Atoms) -> tuple[npt.NDArray[np.int32], npt.NDArray[np.float64]]:
+def find_MO_bonds(atoms: ase.Atoms) -> tuple[npt.NDArray[np.int32], npt.NDArray[np.float64]]:
     ''' Finds B-site cation-anion pairs and the vectors connecting them.
+    Only bonds centered at the anions are returned.
 
     Parameters:
-        xtl (ase.Atoms)
+        atoms: ase.Atoms
 
     Returns:
-        bonds (Tuple[ndarray[int], ndarray[float]])
+        (bond_pairs, bond_distances)
+        bond_pairs: npt.NDArray[int]
+        bond_distances: npt.NDArray[float]
     '''
-    xtl_std = standardize_atoms(xtl)
+    atoms_std = standardize_atoms(atoms)
     # For each nearest neighbor ("bonded") pair:
     #   i: ion 1 index
     #   j: ion 2 index
     #   D: distance vector
-    cutoff = ase.neighborlist.natural_cutoffs(xtl_std)
-    i, j, D = ase.neighborlist.neighbor_list('ijD', xtl_std, cutoff=cutoff)
+    cutoff = ase.neighborlist.natural_cutoffs(atoms_std)
+    i, j, D = ase.neighborlist.neighbor_list('ijD', atoms_std, cutoff=cutoff)
 
     # find cation and anion indices
     cation_indices = np.where(
-            [atom in cations for atom in xtl_std.get_chemical_symbols()]
+            [atom in cations for atom in atoms_std.get_chemical_symbols()]
             )[0]
     anion_indices = np.where(
-            [atom in anions for atom in xtl_std.get_chemical_symbols()]
+            [atom in anions for atom in atoms_std.get_chemical_symbols()]
             )[0]
 
     # enumerate valid B-site cation-anion bonds
@@ -99,13 +106,13 @@ def find_MO_bonds(xtl: ase.Atoms) -> tuple[npt.NDArray[np.int32], npt.NDArray[np
 
     return (np.array(bond_pairs), np.array(bond_distances))
 
-def pseudocubic_lattice_vectors(xtl: ase.Atoms) -> tuple[npt.NDArray, npt.NDArray]:
+def pseudocubic_lattice_vectors(atoms: ase.Atoms) -> tuple[npt.NDArray, npt.NDArray]:
     ''' Takes orthorhombic perovskite and returns its pseudocubic lattice
     vectors as unit vectors and lengths.
-    Assumes that the long axis (c) is xtl.cell[2]
+    Assumes that the long axis (c) is atoms.cell[2]
 
     Parameters:
-    xtl: ase.Atoms
+    atoms: ase.Atoms
 
     Returns: (pseudo_unit_vectors, lengths)
     pseudo_unit_vectors: np.ndarray
@@ -115,25 +122,36 @@ def pseudocubic_lattice_vectors(xtl: ase.Atoms) -> tuple[npt.NDArray, npt.NDArra
     '''
 
     # compute pseudocubic lattice vectors a_p and b_p
-    a_pseudo = (xtl.cell.array[0] + xtl.cell.array[1]) / np.sqrt(2)
-    b_pseudo = (xtl.cell.array[0] - xtl.cell.array[1]) / np.sqrt(2)
+    a_pseudo = (atoms.cell.array[0] + atoms.cell.array[1]) / np.sqrt(2)
+    b_pseudo = (atoms.cell.array[0] - atoms.cell.array[1]) / np.sqrt(2)
 
     pseudo_unit_vectors = np.array([
         a_pseudo / np.linalg.norm(a_pseudo),
         b_pseudo / np.linalg.norm(b_pseudo),
-        xtl.cell.array[2] / np.linalg.norm(xtl.cell.array[2])
+        atoms.cell.array[2] / np.linalg.norm(atoms.cell.array[2])
         ])
 
     lengths = np.array([
         np.linalg.norm(a_pseudo),
         np.linalg.norm(b_pseudo),
-        xtl.cell.lengths()[2]
+        atoms.cell.lengths()[2]
         ])
 
     return (pseudo_unit_vectors, lengths)
 
 def vector_projection(vector: npt.ArrayLike, normal: npt.ArrayLike) -> npt.NDArray:
-    ''' Projects vector onto the plane defined by the normal vector '''
+    ''' Projects vector onto the plane defined by the normal vector.
+
+    The vector projection is given by:
+        vector - DOT_PRODUCT(vector, normal) * normal
+
+    Parameters:
+        vector: npt.ArrayLike
+        normal: npt.ArrayLike
+
+    Returns:
+        proj: npt.NDArray
+    '''
     vector = np.asarray(vector)
     normal = np.asarray(normal)
 
@@ -143,3 +161,33 @@ def vector_projection(vector: npt.ArrayLike, normal: npt.ArrayLike) -> npt.NDArr
     proj = vector - (np.dot(vector, normal)) * normal
 
     return proj
+
+def bond_angle(bond1: npt.ArrayLike, bond2: npt.ArrayLike,
+        proj_normal: npt.ArrayLike) -> float:
+    '''
+    Computes bond angle projected onto a plane.
+
+    Parameters:
+        bond1: npt.ArrayLike
+                Vector of first bond
+        bond2: npt.ArrayLike
+                Vector of second bond
+        proj_normal: npt.ArrayLike
+                Normal vector to the projection plane
+
+    Returns:
+        angle: float
+                bond angle in degrees
+    '''
+
+    proj1 = vector_projection(bond1, proj_normal)
+    proj2 = vector_projection(bond2, proj_normal)
+    l1 = np.linalg.norm(proj1)
+    l2 = np.linalg.norm(proj2)
+
+    angle = np.arccos(
+            np.dot(proj1, proj2) / (l1 * l2)
+            )
+    angle = np.rad2deg(angle)
+
+    return angle
