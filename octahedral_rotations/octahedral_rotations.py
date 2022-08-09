2e4664ca82329c4c7044d75213e1b55830a30d80
Sean <koysean@gmail.com>
2022-08-09 15:47:52 -0500

    Modified mean angle definitions to be actual rotation/tilt angles instead of bond angles

--- octahedral_rotations/octahedral_rotations.py
+++ octahedral_rotations/octahedral_rotations.py
@@ -106,11 +106,14 @@ class OctahedralRotations():
 
         # use temp to reference each type of angle
         temp = self.site_bond_angles[:,0] #tilt_a
-        self.mean_tilt_a = np.mean(temp[~np.isclose(temp, 0, atol=0.001)])
+        self.mean_tilt_a = (180 - np.mean(temp[~np.isclose(temp, 0,
+            atol=0.001)])) / 2
         temp = self.site_bond_angles[:,1] #tilt_b
-        self.mean_tilt_b = np.mean(temp[~np.isclose(temp, 0, atol=0.001)])
+        self.mean_tilt_b = (180 - np.mean(temp[~np.isclose(temp, 0,
+            atol=0.001)])) / 2
         temp = self.site_bond_angles[:,2] #rotation
-        self.mean_rotation = np.mean(temp[~np.isclose(temp, 0, atol=0.001)])
+        self.mean_rotation = (180 - np.mean(temp[~np.isclose(temp, 0,
+            atol=0.001)])) / 2
 
     def get_pseudocubic_lattice(self) -> tuple[npt.NDArray, npt.NDArray]:
         ''' Returns pseudocubic lattice vectors as unit vectors and lengths.
